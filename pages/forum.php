5a4da18988f3c537c1453e67aba174be35130cb7
l3ernard <l3ernardz@gmail.com>
2019-09-24 14:08:05 +0200

    Forum wip
    Forum styling soon backend ^_^

--- pages/forum.php
+++ pages/forum.php
@@ -1,1 +1,17 @@
-forum.php
+<table class="forumTable">
+    <tr class="cat"><td>General</td><td>Latest Post</td><td>Date</td><td>Author</td></tr>
+    <tr><td>Discussion</td><td>Why is this?</td><td></td><td></td></tr>
+    <tr><td>Jaenzo</td><td>neen?</td><td></td><td></td></tr>
+    <tr><td>Garbage</td><td>trash?</td><td></td><td></td></tr>
+
+    <tr class="cat"><td>Gaming</td><td>Latest Post</td><td>Date</td><td>Author</td></tr>
+    <tr><td>Discussion</td><td>csgo sucks</td><td></td><td></td></tr>
+    <tr><td>Share ur shit</td><td>3660 noscope?</td><td></td><td></td></tr>
+    <tr><td>Garbage</td><td>w00t?</td><td></td><td></td></tr>
+
+
+
+
+
+
+</table>
