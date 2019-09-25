25f5e824f91cd03defa1983d0c00548999baae32
l3ernard <l3ernardz@gmail.com>
2019-09-25 15:02:16 +0200

    Added forum files wip
    added forum files and updated database

--- pages/forum.php
+++ pages/forum.php
@@ -1,17 +1,15 @@
-<table class="forumTable">
-    <tr class="cat"><td>General</td><td>Latest Post</td><td>Date</td><td>Author</td></tr>
-    <tr><td>Discussion</td><td>Why is this?</td><td></td><td></td></tr>
-    <tr><td>Jaenzo</td><td>neen?</td><td></td><td></td></tr>
-    <tr><td>Garbage</td><td>trash?</td><td></td><td></td></tr>
+<?php
+if (!isset($_GET['f'])) {
+    include('forum/overview.php');
+} else {
 
-    <tr class="cat"><td>Gaming</td><td>Latest Post</td><td>Date</td><td>Author</td></tr>
-    <tr><td>Discussion</td><td>csgo sucks</td><td></td><td></td></tr>
-    <tr><td>Share ur shit</td><td>3660 noscope?</td><td></td><td></td></tr>
-    <tr><td>Garbage</td><td>w00t?</td><td></td><td></td></tr>
+    if (!file_exists("pages/forum/" . $_GET['f'] . ".php")) {
+        header("Location: forum/overview");
+    } else {
+        include("pages/forum/" . $_GET['f'] . ".php");
+    }
 
-
-
-
+}
 
 
-</table>
+
---
+++ pages/forum/admin.php
@@ -0,0 +1,40 @@
+<?php
+
+if(!isset($_SESSION['login'])){
+
+
+} else {
+    include'./include/admin.inc.php';
+
+    $userManager = new UserManager();
+    $cur_info = $userManager->user_info();
+
+    $cur_username = $cur_info['username'];
+    $cur_user_level = $cur_info['level'];
+
+    if ($cur_user_level < 10) {
+        echo 'Not authorized.';
+
+    } else {
+
+        ?>
+
+        <div id="admin-panel">
+            <div id="page-header">
+                <div id="page-title">
+                    <h1>Admin Panel</h1>
+                </div>
+
+            </div>
+            <div id="admincontent">
+                <table >
+                    <tr><td>Categories</td></tr>
+
+                </table>
+
+            </div>
+        </div>
+        <?php
+    }
+}
+?>
